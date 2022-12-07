de5faab118ecc7f7aae64eae54d9436f91d143dd
Caike Bispo <caikbispo@hotmail.com>
2022-12-06 23:39:58 -0300

    FEAT(Main) Praticando o uso do let

--- src/br/com/alura/bytebank/main.kt
+++ src/br/com/alura/bytebank/main.kt
@@ -16,6 +16,12 @@ fun main() {
 
     println(qualquer.nome)
 
+    /*
+    * Ao explorar a assinatura do let(), vimos que foi utilizada a palavra chave inline.
+    * O inline é uma técnica de otimização mais avançada que não veremos durante o curso,
+    * portanto, se tiver interesse em saber sobre o assunto, confira esta página da documentação.
+    * */
+
 
 }
 
