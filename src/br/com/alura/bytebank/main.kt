ad7957e87389ede0c82197a04a06a673174c9c3a
Caike Bispo <caikbispo@hotmail.com>
2022-12-08 22:58:52 -0300

    FEAT(Main) Implementando códigos com o let

--- src/br/com/alura/bytebank/main.kt
+++ src/br/com/alura/bytebank/main.kt
@@ -1,4 +1,7 @@
 package br.com.alura.bytebank
+
+import br.com.alura.bytebank.modelo.Endereco
+
 fun main() {
 
     val nameWithGreet = "Caike Evangelista Bispo".let {arg ->
@@ -21,6 +24,17 @@ fun main() {
     * O inline é uma técnica de otimização mais avançada que não veremos durante o curso,
     * portanto, se tiver interesse em saber sobre o assunto, confira esta página da documentação.
     * */
+
+    Endereco("Maeda", 777, "Vila Virgínia", "Suzano", "SP", "Apartamento 901").let { endereco ->
+        "Rua: ${endereco.logradouro}, Bairro: ${endereco.bairro}, Cidade: ${endereco.cidade}".toUpperCase()
+    }.let(::println)
+
+    listOf<Endereco>(Endereco(logradouro = "João goular",complemento = "Apartamento 901"),
+        Endereco(logradouro = "João Pekny", complemento = "Apartamento da praia"),
+        Endereco(logradouro = "Gastão Vidigal", complemento = "")
+    ).filter { endereco ->  endereco.complemento.isEmpty()}
+    .let (::println)
+
 
 
 }
