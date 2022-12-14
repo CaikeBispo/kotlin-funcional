32115408c7429a30f2253a3fd72504417246a825
Caike Bispo <caikbispo@hotmail.com>
2022-12-13 23:29:03 -0300

    FEAT(main) Testando with

--- src/br/com/alura/bytebank/main.kt
+++ src/br/com/alura/bytebank/main.kt
@@ -1,5 +1,27 @@
 package br.com.alura.bytebank
+
+import br.com.alura.bytebank.modelo.Endereco
+
 fun main() {
 
+    testandoWith()
+
+
+}
+
+private fun testandoWith() {
+    with(
+        Endereco(
+            "Rua Caratinga",
+            162,
+            "Jardim São Caetano",
+            "Carira",
+            "Sergipe",
+            "08585-085",
+            "Casa 789"
+        )
+    ) {
+        completo()
+    }.let(::println)
 }
 
