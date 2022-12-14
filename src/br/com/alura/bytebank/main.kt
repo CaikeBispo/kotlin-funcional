29bdb1c90a16890462b0c538bf888e54dc565a5f
Caike Bispo <caikbispo@hotmail.com>
2022-12-13 23:15:27 -0300

    FEAT(Main) Removendo importações desnecessárias

--- src/br/com/alura/bytebank/main.kt
+++ src/br/com/alura/bytebank/main.kt
@@ -1,11 +1,4 @@
 package br.com.alura.bytebank
-
-import br.com.alura.bytebank.modelo.Autenticavel
-import br.com.alura.bytebank.modelo.Endereco
-import br.com.alura.bytebank.teste.TestaHOF
-import br.com.alura.bytebank.teste.TestaScopeFunctions
-
-
 fun main() {
 
 }
