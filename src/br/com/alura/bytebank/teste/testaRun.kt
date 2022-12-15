acf96117c6bd768d27496732c66a9c4be0963da7
Caike Bispo <caikbispo@hotmail.com>
2022-12-14 23:59:41 -0300

    FEAT(testaRun) Organizando testes com run

--- src/br/com/alura/bytebank/teste/testaRun.kt
+++ src/br/com/alura/bytebank/teste/testaRun.kt
@@ -1,4 +1,32 @@
 package br.com.alura.bytebank.teste
 
-class testaRun {
+import br.com.alura.bytebank.modelo.Cliente
+import br.com.alura.bytebank.modelo.ContaPoupanca
+
+fun testaRun(){
+    val taxaAnual:Double = 0.05
+    val taxaMensal:Double = taxaAnual / 12
+
+    val poupanca = Cliente(nome = "Caike Bispo", cpf = "123.123.123-12", senha = 123456).let { cliente ->
+        ContaPoupanca(cliente, 1218984)
+    }
+
+    val rendimentoMensal = poupanca.run {
+        deposita(1000.0)
+        saldo * taxaMensal
+    }
+
+    println("O rendimento será: $rendimentoMensal")
+
+    val simulacaoRendimentoAnual = rendimentoMensal.run {
+        var saldo = poupanca.saldo
+
+        repeat(12) {
+            saldo += saldo * taxaMensal
+        }
+        saldo
+    }
+
+    println("Saldo após um ano: $simulacaoRendimentoAnual")
+
 }
