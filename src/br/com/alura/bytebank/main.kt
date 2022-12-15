32d590a9b39014d42164262c576fd13f377d466d
Caike Bispo <caikbispo@hotmail.com>
2022-12-15 00:00:07 -0300

    FEAT(testaWith) Organizando testes com with

--- src/br/com/alura/bytebank/main.kt
+++ src/br/com/alura/bytebank/main.kt
@@ -1,56 +1,12 @@
 package br.com.alura.bytebank
 
-import br.com.alura.bytebank.modelo.Cliente
-import br.com.alura.bytebank.modelo.ContaPoupanca
-import br.com.alura.bytebank.modelo.Endereco
-
 fun main() {
-    testandoRun()
-
-}
-
-fun testandoRun(){
-    val taxaAnual:Double = 0.05
-    val taxaMensal:Double = taxaAnual / 12
-
-    val poupanca = Cliente(nome = "Caike Bispo", cpf = "123.123.123-12", senha = 123456).let { cliente ->
-        ContaPoupanca(cliente, 1218984)
-    }
-
-    val rendimentoMensal = poupanca.run {
-        deposita(1000.0)
-        saldo * taxaMensal
-    }
-
-    println("O rendimento será: $rendimentoMensal")
-
-    val simulacaoRendimentoAnual = rendimentoMensal.run {
-        var saldo = poupanca.saldo
-
-        repeat(12) {
-           saldo += saldo * taxaMensal
-        }
-        saldo
-    }
-
-    println("Saldo após um ano: $simulacaoRendimentoAnual")
 
 }
 
 
-fun testandoWith() {
-    with(
-        Endereco(
-            "Rua Caratinga",
-            162,
-            "Jardim São Caetano",
-            "Carira",
-            "Sergipe",
-            "08585-085",
-            "Casa 789"
-        )
-    ) {
-        completo()
-    }.let(::println)
-}
+
+
+
+
 
--- src/br/com/alura/bytebank/teste/testaWith.kt
+++ src/br/com/alura/bytebank/teste/testaWith.kt
@@ -1,4 +1,19 @@
 package br.com.alura.bytebank.teste
 
-class testaWith {
+import br.com.alura.bytebank.modelo.Endereco
+
+fun testaWith() {
+    with(
+        Endereco(
+            "Rua Caratinga",
+            162,
+            "Jardim São Caetano",
+            "Carira",
+            "Sergipe",
+            "08585-085",
+            "Casa 789"
+        )
+    ) {
+        completo()
+    }.let(::println)
 }
