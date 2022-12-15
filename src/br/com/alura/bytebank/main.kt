b8b89bbfe302f89fb5add104edc67edf286e710b
Caike Bispo <caikbispo@hotmail.com>
2022-12-14 23:46:22 -0300

    FEAT(main) Realizando testes com run

--- src/br/com/alura/bytebank/main.kt
+++ src/br/com/alura/bytebank/main.kt
@@ -1,15 +1,44 @@
 package br.com.alura.bytebank
 
+import br.com.alura.bytebank.modelo.Cliente
+import br.com.alura.bytebank.modelo.ContaPoupanca
 import br.com.alura.bytebank.modelo.Endereco
 
 fun main() {
-
-    testandoWith()
-
+    testandoRun()
 
 }
 
-private fun testandoWith() {
+fun testandoRun(){
+    val taxaAnual:Double = 0.05
+    val taxaMensal:Double = taxaAnual / 12
+
+    val poupanca = Cliente(nome = "Caike Bispo", cpf = "123.123.123-12", senha = 123456).let { cliente ->
+        ContaPoupanca(cliente, 1218984)
+    }
+
+    val rendimentoMensal = poupanca.run {
+        deposita(1000.0)
+        saldo * taxaMensal
+    }
+
+    println("O rendimento será: $rendimentoMensal")
+
+    val simulacaoRendimentoAnual = rendimentoMensal.run {
+        var saldo = poupanca.saldo
+
+        repeat(12) {
+           saldo += saldo * taxaMensal
+        }
+        saldo
+    }
+
+    println("Saldo após um ano: $simulacaoRendimentoAnual")
+
+}
+
+
+fun testandoWith() {
     with(
         Endereco(
             "Rua Caratinga",
